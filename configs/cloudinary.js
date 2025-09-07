84ea95e89dc93f377ef53f93091d7564ed2ff97b
Neel2377 <riderh911@gmail.com>
2025-09-07 15:20:16 +0530

    update

--- configs/cloudinary.js
+++ configs/cloudinary.js
@@ -1,5 +1,12 @@
 const cloudinary = require('cloudinary').v2;
 
+require('dotenv').config();
+
+if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_KEY || !process.env.CLOUDINARY_SECRET) {
+  console.error('Cloudinary configuration error: Missing environment variables.');
+  process.exit(1);
+}
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key:    process.env.CLOUDINARY_KEY,
